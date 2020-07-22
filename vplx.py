18edabc152b3d0c68c76e6a4cfbd34b98a2ea01a
Zhgx <2523849766@qq.com>
2020-07-22 10:04:07 +0800

    complete the test successful

--- vplx.py
+++ vplx.py
@@ -416,8 +416,8 @@ class VplxCrm(object):
         #                          f'      Start the iSCSILogicalUnit resource {self.lu_name}')
         result_cmd = s.get_ssh_cmd(SSH, unique_str, cmd, oprt_id)
         if result_cmd['sts']:
-            if self.cyclic_check_crm_status(self.lu_name,'Started')
-            s.pwl(f'Succeed in starting up iSCSILogicaLUnit "{self.lu_name}"', 3, oprt_id, 'finish')
+            if self.cyclic_check_crm_status(self.lu_name,'Started'):
+                s.pwl(f'Succeed in starting up iSCSILogicaLUnit "{self.lu_name}"', 3, oprt_id, 'finish')
             # print('    ISCSI LUN start successful')
             # self.logger.write_to_log(
             #     'T', 'INFO', 'info', 'finish', '', '    ISCSI LUN start successful')
@@ -560,7 +560,7 @@ class VplxCrm(object):
 
     def del_all(self, crm_to_del_list):
         # print('start to delete crm resource:')
-        s.pwl('start to delete crm resource:',0,'','delete')
+        s.pwl('start to delete crm resource',0,'','delete')
         if crm_to_del_list:
             for res_name in crm_to_del_list:
                 self.crm_del(res_name)
